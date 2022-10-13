b033ab939c845c86ba548d13bcda0241746247c0
Ivan_home <svinpauk78@gmail.com>
2022-10-13 22:07:26 +0300

    add icons facebook

--- resources/views/auth/partials/socials.blade.php
+++ resources/views/auth/partials/socials.blade.php
@@ -1,5 +1,7 @@
 <div class="form__form-group -submit form-group">
     <span>Войти через: &nbsp;</span>
-    <a href="{{ route('social.login', ['driver' => 'facebook']) }}" style="margin-right: 10px;">Facebook</a>
-    <a href="{{ route('social.login', ['driver' => 'apple']) }}">Apple</a>
+    <a href="{{ route('social.login', ['driver' => 'facebook']) }}" style="margin-right: 10px;">
+       Facebook
+    </a>
+{{--    <a href="{{ route('social.login', ['driver' => 'apple']) }}">Apple</a>--}}
 </div>
