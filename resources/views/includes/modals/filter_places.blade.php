e7d050121ae07f39de6eb71fc2e3d6512719d736
Ivan_home <svinpauk78@gmail.com>
2022-10-16 11:31:55 +0300

    Change AppServiceProvider, added templates, add places-modal, people-modal

---
+++ resources/views/includes/modals/filter_places.blade.php
@@ -0,0 +1,68 @@
+<div class="filter -places" id="slideout-places">
+    <form class="form" action="#" method="post">
+        <div class="container">
+            <div class="form__top">
+                <div class="form__inner">
+                    <div class="form__left">
+                        <div class="form__title">Поиск мест</div>
+                    </div>
+                    <div class="form__right">
+                        <div class="form__form-group form-group">
+                            <div class="form__message">Введите фамилию, имя, отчество
+                                профиля, который вы хотите найти. Если
+                                вы не уверенны в датах, укажите примерные.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="form__items">
+                <div class="form__inner">
+                    <div class="form__left">
+                        <div class="form__row row">
+                            <div class="form__col -name col">
+                                <div class="form__form-group form-group">
+                                    <label class="form__label" for="filter-places-name">Название:</label>
+                                    <div class="form__input-container">
+                                        <input class="form__input form-control"
+                                               type="text" id="filter-places-name"
+                                               name="NAME" placeholder=" ">
+                                        <button class="form__button -close close"
+                                                type="button"></button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form__right">
+                        <div class="form__row row">
+                            <div class="form__col -address col">
+                                <div class="form__form-group form-group">
+                                    <label class="form__label"
+                                           for="filter-places-address">Местоположение:</label>
+                                    <div class="form__input-container">
+                                        <input class="form__input form-control"
+                                               type="text" id="filter-places-address"
+                                               name="ADDRESS" placeholder=" ">
+                                        <button class="form__button -close close"
+                                                type="button"></button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form__col -submit col">
+                                <div class="form__form-group -submit form-group">
+                                    <button
+                                        class="form__button -submit btn btn-primary btn-lg"
+                                        type="submit">
+                                        <span>Показать</span>
+                                        <span>2 799</span>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
