0bfce10646655437c9efb67ddf1807dca78d1bb4
Ivan_home <svinpauk78@gmail.com>
2022-10-28 20:39:39 +0300

    cemetery map via npm

--- resources/views/cemetery/map.blade.php
+++ resources/views/cemetery/map.blade.php
@@ -50,9 +50,3 @@
 
 @endsection
 
-@section('scripts')
-    <script
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCO1W6X1LgXeZzrDSNL6YMbZm9Z9NAPH5Y&callback=initMap&v=weekly"
-        defer
-    ></script>
-@endsection
