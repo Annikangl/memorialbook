01d7dd32db3e6ee1f0210f511ce82e18f59a52cf
Annikangl <svinpauk78@gmail.com>
2022-11-15 21:36:34 +0300

    Add factory for Community, change community/script.js

--- database/factories/Community/CommunityFactory.php
+++ database/factories/Community/CommunityFactory.php
@@ -9,12 +9,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class CommunityFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition()
+    public function definition(): array
     {
         return [
             'owner_id' => 1,
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,7 @@
     <script data-skip-moving="true">
         window.app = window.app || {};
         window.app.globalConfig = {
-            baseUrl: 'http://memorialbook.loc:8080/',
+            baseUrl: 'http://memorialbook.loc/',
             relPath: './',
             assetsPath: '/assets/media/',
             gmapsApikey: 'AIzaSyCO1W6X1LgXeZzrDSNL6YMbZm9Z9NAPH5Y'
