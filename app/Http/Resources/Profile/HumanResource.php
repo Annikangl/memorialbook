ba7ca0881c3ebdb7fc504e50aa92fe68bb2b6a98
Annikangl <svinpauk78@gmail.com>
2023-10-25 21:07:31 +0300

    fix(nginx conf, HumanResource): Add CORS Allows, change HumanResource -> burial_cords field

--- app/Http/Resources/Profile/HumanResource.php
+++ app/Http/Resources/Profile/HumanResource.php
@@ -17,10 +17,7 @@ class HumanResource extends JsonResource
             'full_name' => $this->full_name,
             'date_birth' => $this->year_birth,
             'date_death' => $this->year_death,
-            'burial_cord' => [
-                'lat' => $this->latitude,
-                'lng' => $this->longitude,
-            ],
+            'burial_cord' => $this->burial_coords,
             'avatar' => $this->getFirstMediaUrl('avatars', 'thumb')
         ];
     }
