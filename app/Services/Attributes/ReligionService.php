25031d398dd79abf773852acf11b87f1ccfe3805
Annikangl <svinpauk78@gmail.com>
2024-02-10 15:51:36 +0400

    feat(services: add religion service): Add ReligionService

---
+++ app/Services/Attributes/ReligionService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Services\Attributes;
+
+use App\Models\Profile\Human\Religion;
+
+class ReligionService
+{
+    /**
+     * Create a new religion
+     * @param string $title
+     * @return Religion
+     */
+    public function create(string $title): Religion
+    {
+        return Religion::query()->create(['title' => $title]);
+    }
+
+    /**
+     * Delete a religion
+     * @param Religion $religion
+     * @return bool|null
+     */
+    public function delete(Religion $religion): ?bool
+    {
+        return $religion->delete();
+    }
+}
